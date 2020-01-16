ce6dc04a7366cc5d837512958b9a625513428b46
Li Haofei <1146953160@qq.com>
2020-01-16 14:38:02 +0800

    修改cpu检测

--- monitor.sh
+++ monitor.sh
@@ -10,9 +10,12 @@ echo $time >> monitor.log
 used=$(free -m | awk '{if(NR==2){print $3}}')
 echo "内存使用率为 `expr $used \* 100 \/ $total` %" >> monitor.log
 
-cpu_used=$(top -b| head -n 3 | awk  '{if(NR==3){print $2}}')
+cpu_total=100.0
+cpu_free=$(top -b -n 2 | grep Cpu | tail -n 1 | awk '{print $8}')
+cpu_used=$(echo "$cpu_total - $cpu_free"|bc)
 
 echo "cpu使用率为 $cpu_used %" >> monitor.log
 sleep 5
 find -mtime +3 -type f -name "monitor.log" | xargs rm -f
 done
+
